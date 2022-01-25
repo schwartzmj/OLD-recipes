39be5f7aa8a47ecb2313c7f6446ec29ecf0c43c1
Matt Schwartz <admin@schwartzmj.com>
2022-01-24 22:18:45 -0700

    add sorting logic to create a parent/child toc link array

--- components/RecipeNav.vue
+++ components/RecipeNav.vue
@@ -1,15 +1,24 @@
 <template>
-  <ul class="fixed top-0 right-0 p-4 w-full flex flex-wrap items-center space-x-3 z-50 bg-stone-200 text-stone-600">
-    <li
-      v-for="link of toc"
-      :key="link.id"
-      :id="link.id + '-nav'"
-      class="p-1"
-      :class="{ '': link.depth === 2, 'text-sm': link.depth === 3 }"
-    >
-      <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
-    </li>
-  </ul>
+  <div class="hidden md:block">
+    <ul class="fixed top-0 right-0 p-4 h-full w-32 flex flex-wrap flex-col divide-y-2 divide-stone-300 z-50 bg-stone-200/60 text-stone-600">
+      <li
+        v-for="link of tocLinks"
+        :key="link.id"
+        class="w-full p-1 text-base"
+      >
+        <NuxtLink :to="`#${link.id}`" :id="`${link.id}-nav`">{{ link.text }}</NuxtLink>
+        <ul>
+          <li
+            v-for="child of link.children"
+            :key="child.id"
+            class="p-1 text-xs"
+          >
+            <NuxtLink :to="`#${child.id}`" :id="`${child.id}-nav`">{{ child.text }}</NuxtLink>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -20,29 +29,35 @@ export default {
       required: true
     }
   },
+  computed: {
+    tocLinks() {
+      // Loop over ToC links. If link has depth of 2, start a new 'ToC parent' (h2 tag). If next link has depth of 3, add it to that parent's children array. If next link has depth of 2, start a new 'ToC parent'. If next link has depth of 3, add it to that parent's children array (and so on).
+      // In other words, it is an array of "h2" tags with their children "h3" tags.
+      const tocLinks = []
+      let currentTocParent = { children: [] }
+      this.toc.forEach(link => {
+        if (link.depth === 2) {
+          if (currentTocParent.children.length > 0 || 'id' in currentTocParent) { // previous link was an h2 tag OR we already have an h2 tag so we should start a new one (previous one had no children)
+            tocLinks.push(currentTocParent)
+          }
+          currentTocParent = { ...link, children: [] }
+        } else {
+          currentTocParent.children.push(link)
+        }
+      })
+      tocLinks.push(currentTocParent) // push final TocParent
+      return tocLinks
+    }
+  },
   mounted() {
+    console.log(this.toc)
     this.toc.forEach(link => {
+      // TODO: Get more accurate scroll spy based on height of anchor's content?
+      // Maybe: https://github.com/janpaepke/ScrollMagic/issues/500#issuecomment-216463444
       const scene = this.$scrollmagic.scene({triggerElement: `#${link.id}`, duration: 500})
-        .setClassToggle(`#${link.id}-nav`, 'bg-cyan-50')
+        .setClassToggle(`#${link.id}-nav`, 'bg-stone-300')
       this.$scrollmagic.addScene(scene)
     })
-    // // build scenes
-    // this.$scrollmagic.scene({triggerElement: "#sec1"})
-    //         .setClassToggle("#high1", "active") // add class toggle
-    //         .addIndicators() // add indicators (requires plugin)
-    //         .addTo(controller);
-    // this.$scrollmagic.scene({triggerElement: "#sec2"})
-    //         .setClassToggle("#high2", "active") // add class toggle
-    //         .addIndicators() // add indicators (requires plugin)
-    //         .addTo(controller);
-    // this.$scrollmagic.scene({triggerElement: "#sec3"})
-    //         .setClassToggle("#high3", "active") // add class toggle
-    //         .addIndicators() // add indicators (requires plugin)
-    //         .addTo(controller);
-    // this.$scrollmagic.scene({triggerElement: "#sec4"})
-    //         .setClassToggle("#high4", "active") // add class toggle
-    //         .addIndicators() // add indicators (requires plugin)
-    //         .addTo(controller);
   }
 }
 </script>
